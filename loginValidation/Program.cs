941e4fe9e1fd61b5a18b281a4052f91f0be2d0c0
Timosha145 <93324319+Timosha145@users.noreply.github.com>
2021-11-30 13:34:56 +0200

    update
    3 tries added

--- loginValidation/Program.cs
+++ loginValidation/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace loginValidation
 {
@@ -8,22 +8,28 @@ namespace loginValidation
         {
             string login = "admin";
             string password = "pass1234";
+            int try1 = 3;
 
             string userName, userPass;
 
-            Console.WriteLine("Enter your username: ");
-            userName = Console.ReadLine();
-            Console.WriteLine("Enter your password: ");
-            userPass = Console.ReadLine();
-
-            if(login == userName && password == userPass)
+            while (try1 != 0)
             {
-                Console.WriteLine("Welcome!");
+                Console.WriteLine("Enter your username: ");
+                userName = Console.ReadLine();
+                Console.WriteLine("Enter your password: ");
+                userPass = Console.ReadLine();
+                try1-=1;
+                if(login == userName && password == userPass)
+                {
+                    Console.WriteLine("Welcome!");
+                }
+                else if(try1>0)
+                {
+                    Console.WriteLine($"More {try1} tries!");
+                }
+                Console.WriteLine();
             }
-            else
-            {
-                Console.WriteLine("Oops. Something went wrong!");
-            }
+            Console.WriteLine("Oops. Something went wrong!");
         }
     }
 }
